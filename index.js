667b04713fa16f34f239e6b59ab310667964afc5
F1rman <ihorkharchyshyn@gmail.com>
2024-10-16 17:08:41 +0300

    fixes

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ async function init() {
 
         }
         const subcategory = subcategories.find(subcat => subcat.id === hymns[i].subcategory);
-        if (subcategory !== undefined) {
+        if (subcategory !== undefined && subcategory.category === category.id) {
             hymnObj['subcategory'] = {
                 id: subcategory.id,
                 name: subcategory.name,
@@ -148,7 +148,7 @@ async function init() {
                     name: tune.name.trim(),
                     midiUrl: tune.midi_src?.replace('data/midi/','').replace('.mid',''),
                     mp3Url: tune.mp3_src?.replace('data/mp3/','').replace('.mp3',''),
-                    isPremium: tune.access,
+                    isPremium: tune.access == 1 ? true : false,
                     isDownloading: false,
                     isDownloaded: false,
                     pitch: 0,
@@ -176,64 +176,6 @@ async function init() {
         }
     });
 
-
-    //     // const books = await getBooks();
-
-    //     // for (let i = 0; i < books.length; i++) {
-    //     //     const verses = await getVerses(books[i].book_number);
-    //     //     const chapters = {
-
-    //     //     }
-    //     //     for (var item of verses) {
-    //     //         if (!chapters[item.chapter]) {
-    //     //             chapters[item.chapter] = [];
-    //     //         }
-    //     //         const trimPB = item.text.replace('<pb/>', '');
-    //     //         const trimedStrongs = trimPB.replace(/<S>.*?<\/S>/g, "");
-    //     //         const trimeAllTags = trimedStrongs.replace(/<.*?>/g, "");
-    //     //         chapters[item.chapter].push(trimeAllTags);
-    //     //     }
-    //     //     bible.books.push({
-    //     //         [books[i].long_name]: chapters
-    //     //     });
-    //     // }
-
-    //     // console.log(bible.books.length);
-
-
-    //     const xml = builder.create('XMLBIBLE', { encoding: 'UTF-8', version: '1.0', standalone: true })
-    //         .att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
-
-
-    //     // const books = await getBooks();
-
-
-    //     // // to xml
-    //     // for (let i = 0; i < bible.books.length; i++) {
-    //     //     const book = bible.books[i];
-    //     //     const bookName = Object.keys(book)[0];
-    //     //     const bookChapters = book[bookName];
-    //     //     const bibleBook = xml.ele('BIBLEBOOK', { bnumber: i + 1, bname: bookName });
-
-    //     //     for (let chapter in bookChapters) {
-    //     //         const chapterElement = bibleBook.ele('CHAPTER', { cnumber: chapter });
-    //     //         const verses = bookChapters[chapter];
-    //     //         for (let verse in verses) {
-    //     //             chapterElement.ele('VERS', { vnumber: parseInt(verse)+1 }, verses[verse]);
-    //     //         }
-    //     //     }
-    //     // }
-
-    //     // const xmlString = xml.end({ pretty: true });
-
-    //     // const outputPath = path.join(__dirname, 'bible.xml');
-    //     // // Write the XML string to a file
-    //     // fs.writeFile(outputPath, xmlString, (err) => {
-    //     //     if (err) {
-    //     //         console.error('Error writing XML file: ', err.message);
-    //     //     } else {
-    //     //         console.log(`XML file saved successfully`);
-    //     //     }
-    //     // });
+ 
 
 }
